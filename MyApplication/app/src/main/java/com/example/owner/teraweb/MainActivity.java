23cd891756b28823d172d757207bcaace4f903cf
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2016-03-10 09:14:30 -0500

    Basic sample URL navigation functional

--- MyApplication/app/src/main/java/com/example/owner/teraweb/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/owner/teraweb/MainActivity.java
@@ -88,23 +88,13 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        /*
+        WebView myWebView = (WebView) findViewById(R.id.webview);
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.google) {
+            myWebView.loadUrl("http://google.com");
         }
-        */
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
